b83dfcc0090919912c17ab186be18ffd36baecc2
Robson De Andrade <theairelibre@protonmail.com>
2022-06-10 10:52:54 -0300

    create class Calculator

---
+++ M2S02/calculadora.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Criar uma aplicação de console para simular de maneira simples uma calculadora.
+Entrada: Receber dois valores, Informação de Operação.
+Saída: Baseado nos valores e a operação mostrar o valor.
+Ter uma opção no menu para parar a aplicação, 
+senão enquanto isso, após o resultado mostra para selecionar a outra operação.
+
+3. Crie um menu para escolher a operação Adição, Subtração, Multiplicação, Divisão e uma opção SAIR para interromper o programa.
+- Listar opções junto com o caractere de seleção.
+
+4. Receber informação de operação e Informar dois números
+- Receber a opção e ir para a validação selecionada
+- Receber dois valores
+- Enquanto o caractere da opção SAIR não for selecionada deve continuar a escolha da operação após o resultado.
+
+5. Criar métodos para cada operação, recebendo por parâmetro os dois valores e retornando o resultado dessa operação. 
+- deve mostrar o resultado do processamento baseado nas entradas e a operação informada
+*/
+
+using System;
+
+class Calculator { 
+    
+}
