8b4b69439dd5b82a251debdece7922b8666061d2
Robson De Andrade <theairelibre@protonmail.com>
2022-06-11 16:04:05 -0300

    Create Methods static void

--- M2S02/calculadora.Console/Program.cs
+++ M2S02/calculadora.Console/Program.cs
@@ -19,5 +19,109 @@ retornando o resultado dessa operação.
 - deve mostrar o resultado do processamento baseado nas entradas e a operação informada.
 */
 class Calculator { 
-    
+    static void Main() {
+
+        System.Console.WriteLine("\n\n --------- Calculadora ---------- \n");
+        System.Console.WriteLine("+ Adição");
+        System.Console.WriteLine("- Subtração");
+        System.Console.WriteLine("* Multiplicação");
+        System.Console.WriteLine("/ Divisão\n");
+
+        MenuOperators();
+        
+    }
+    static void ExecuteProgramWhile() {
+        
+    }
+    static void MenuOperators() {
+
+       System.Console.WriteLine("Escolha um operador: ");
+
+       char options;
+       bool value;
+
+       value = Char.TryParse(Console.ReadLine(), out options);
+
+        switch (options) {
+            case '+':
+                Somar();
+                break;
+            case '-':
+                System.Console.WriteLine("Subtração");
+                Subtrair();
+                break;
+            case '*':
+                System.Console.WriteLine("Multiplicação");
+                Multiplicar();
+                break;
+            case '/':
+                System.Console.WriteLine("Divisão");
+                Dividir();
+                break;
+            case 'S':
+                System.Console.WriteLine("Sair");
+                break;
+            default:
+            System.Console.WriteLine("Opção Inválida");
+            break;
+        }
+
+    }
+    static void Somar() {
+        
+        System.Console.WriteLine("Digite o primeiro número: ");
+
+        int number1 = System.Convert.ToInt32(Console.ReadLine());
+
+        System.Console.WriteLine("Digite o segundo número: ");
+
+        int number2 = System.Convert.ToInt32(Console.ReadLine());
+
+        int soma = (number1 + number2);
+
+        System.Console.WriteLine($"O resultado é: {number1} + {number2} = {soma} ");
+    }
+    static void Subtrair() {
+
+        System.Console.WriteLine("Digite o primeiro número: ");
+
+        int number1 = System.Convert.ToInt32(Console.ReadLine());
+
+        System.Console.WriteLine("Digite o segundo número: ");
+
+        int number2 = System.Convert.ToInt32(Console.ReadLine());
+
+        int subtrai = (number1 - number2);
+
+        System.Console.WriteLine($"O resultado é: {number1} - {number2} = {subtrai} ");
+    }
+    static void Dividir() {
+
+        System.Console.WriteLine("Digite o primeiro número: ");
+
+        int number1 = System.Convert.ToInt32(Console.ReadLine());
+
+        System.Console.WriteLine("Digite o segundo número: ");
+
+        int number2 = System.Convert.ToInt32(Console.ReadLine());
+
+        int divide = (number1 / number2);
+
+        System.Console.WriteLine($"O resultado é: {number1} / {number2} = {divide} ");
+    }
+    static void Multiplicar() {
+
+        System.Console.WriteLine("Digite o primeiro número: ");
+
+        int number1 = System.Convert.ToInt32(Console.ReadLine());
+
+        System.Console.WriteLine("Digite o segundo número: ");
+
+        int number2 = System.Convert.ToInt32(Console.ReadLine());
+
+        int multiplica = (number1 * number2);
+
+        System.Console.WriteLine($"O resultado é: {number1} * {number2} = {multiplica} ");
+    }
+
 }
