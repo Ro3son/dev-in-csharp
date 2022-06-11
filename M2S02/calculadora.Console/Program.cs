38d0a403a1c047182a3a152c7d9b8fdcc7a24be8
Robson De Andrade <theairelibre@protonmail.com>
2022-06-10 21:34:48 -0300

    class Calculator

--- M2S02/calculadora.Console/Program.cs
+++ M2S02/calculadora.Console/Program.cs
@@ -5,7 +5,8 @@ Saída: Baseado nos valores e a operação mostrar o valor.
 Ter uma opção no menu para parar a aplicação, 
 senão enquanto isso, após o resultado mostra para selecionar a outra operação.
 
-3. Crie um menu para escolher a operação Adição, Subtração, Multiplicação, Divisão e uma opção SAIR para interromper o programa.
+3. Crie um menu para escolher a operação Adição, Subtração, Multiplicação, Divisão 
+e uma opção SAIR para interromper o programa.
 - Listar opções junto com o caractere de seleção.
 
 4. Receber informação de operação e Informar dois números
@@ -13,12 +14,10 @@ senão enquanto isso, após o resultado mostra para selecionar a outra operaçã
 - Receber dois valores
 - Enquanto o caractere da opção SAIR não for selecionada deve continuar a escolha da operação após o resultado.
 
-5. Criar métodos para cada operação, recebendo por parâmetro os dois valores e retornando o resultado dessa operação. 
-- deve mostrar o resultado do processamento baseado nas entradas e a operação informada
+5. Criar métodos para cada operação, recebendo por parâmetro os dois valores e 
+retornando o resultado dessa operação. 
+- deve mostrar o resultado do processamento baseado nas entradas e a operação informada.
 */
-
-using System;
-
 class Calculator { 
     
 }
