d9738ae3c7972fd792a2a61564196a8f4c3fa55d
Robson De Andrade <theairelibre@protonmail.com>
2022-06-13 09:40:35 -0300

    Create class, enum and methods

---
+++ M2S02/operacao-bancaria.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Crie Menu (Adicionar uma cor para o texto do menu, valor inicial de saldo é 0)
+- operações: consulta saldo, depósito, saque, histórico, sair.
+
+Crie método de consulta de saldo e implemente a lógica necessária e o resultado esperado.
+
+
+*/
+
+class CalculadoraBancaria {
+    public enum Menu { saldo, deposito, saque, historico, sair }
+    static void Main() {
+        MeuMenu();
+
+    }
+    static void MeuMenu() {
+       
+    }
+    static void Consultar() {
+      
+    }
+    static void Depositar() {
+
+    }
+    static void sacar() {
+
+    }
+    static void historicoOperacao() {
+
+    }
+}
