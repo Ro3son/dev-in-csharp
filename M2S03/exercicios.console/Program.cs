a7d1dfbdcdb1b0f0ec85d05f39f07cf5cd5968c2
Robson De Andrade <theairelibre@protonmail.com>
2022-06-16 15:17:46 -0300

    Answers

--- M2S03/exercicios.console/Program.cs
+++ M2S03/exercicios.console/Program.cs
@@ -47,6 +47,7 @@ namespace ex01 {
 }
 
 // Ex 2 - Crie uma classe "Pessoa" com seus atributos
+// Ex 3 - Crie um método para calcular a idade da Pessoa
 
 public class Pessoa {
     string nome;
@@ -64,6 +65,31 @@ public class Pessoa {
 
         this.peso = peso;
     }
-
+    
+    static void Main() {
+      Pessoa idade = new Pessoa("", 1991, 1.70, 65.0);
+      idade.IdadePessoa();
+    }
+    public void IdadePessoa() {
+      int anoAtual = 2022;
+      int idade = (anoAtual - anoDeNascimento);
+      System.Console.WriteLine($"A idade atual é {idade} anos.");
+    }
 }
 
+// Ex 4 - Defina o que é uma atributo.
+// Resposta: Um atributo é uma instância de uma classe.
+
+// Ex 5 - Qual a diferença entre uma propriedade privada e pública?
+// Resposta: A propriedade privada só pode ser acessada pela classe a qual pertence.
+// A propriedade pública pode ser acessada por todas as classes da aplicação;
+
+// Ex 6 - Se você criar 5 objetos de uma classe, quantas vezes o construtor da classe será chamado?
+// Resposta: Apenas uma vez
+
+// Ex 7 - Por que você usa um construtor?
+// Resposta: Para obter valores iniciais.
+// Quando quero definir um construtor adicional e pegar diferentes argumentos.
+
+// Ex 8 - Quando o construtor de uma classe é chamado?
+//Resposta: A qualquer hora que uma instância de uma classe é criada.
