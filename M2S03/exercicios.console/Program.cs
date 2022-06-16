726f221a68cab49a1a234479facd7caac38dce20
Robson De Andrade <theairelibre@protonmail.com>
2022-06-16 11:30:03 -0300

    Classes and Constructor

--- M2S03/exercicios.console/Program.cs
+++ M2S03/exercicios.console/Program.cs
@@ -1,17 +1,21 @@
 ﻿// Ex 1 - Defina uma classe e seus objetos.
-class Funcionario {
-    public string nome;
-    public int anoDeNascimento;
-    public decimal salario;
-    public decimal reajuste;
 
-    public Funcionario(string nome, int anoDeNascimento, decimal salario, decimal reajuste) {
-       this.nome = nome;
-       this.anoDeNascimento = anoDeNascimento;
-       this.salario = salario;
-       this.reajuste = reajuste;
-    }
-    static void Main() {
+using System;
 
+namespace ex01 {
+    class Funcionario {
+        public string nome;
+        public int anoDeNascimento;
+        public decimal salario;
+        public decimal reajuste;
+        public Funcionario(string nome, int anoDeNascimento, decimal salario, decimal reajuste) {
+        this.nome = nome;
+        this.anoDeNascimento = anoDeNascimento;
+        this.salario = salario;
+        this.reajuste = reajuste;
+        }
+        static void Main() {
+
+        }
     }
 }
