671799be1f229e54b80c986c79c83a7533994a4c
Robson De Andrade <theairelibre@protonmail.com>
2022-06-22 20:03:33 -0300

    public class bool field

---
+++ M2S04/exercicios.console/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Ex 1
+Desenvolva uma abstração de uma lâmpada.
+
+Desenvolva uma abstração de uma lâmpada, a qual pode ser ligada e desligada. 
+Também deve ser possível observar o estado da lâmpada (se desligada ou ligada).
+*/
+
+public class Lampada {
+    bool ligada;
+    public void Ligar() {
+       ligada = true;
+    }
+    public void Desligar() {
+       ligada = false;
+
+    }
+}
