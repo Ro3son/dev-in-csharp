d534882f6a718287ce91bddea514af3ccccfd8f7
Robson De Andrade <theairelibre@protonmail.com>
2022-06-24 11:05:47 -0300

    Create new class and properties

--- M2S04/exercicios.console/Program.cs
+++ M2S04/exercicios.console/Program.cs
@@ -1,17 +1,36 @@
-﻿/* Ex 1
-Desenvolva uma abstração de uma lâmpada.
+﻿/* Ex 1, 2
 
 Desenvolva uma abstração de uma lâmpada, a qual pode ser ligada e desligada. 
 Também deve ser possível observar o estado da lâmpada (se desligada ou ligada).
+
+Desenvolva um novo tipo de abstração para a lâmpada de forma a incluir as características de potência e voltagem. 
+Garanta que seja possível tanto ler quanto alterar os valores de potência e voltagem de uma lâmpada. 
+Essa nova classe deve ser usada como um tipo de uma propriedade da classe Lâmpada.
 */
-
 public class Lampada {
-    bool ligada;
-    public void Ligar() {
+   bool ligada;
+   public NovaAbstracao MyAbstraction { get; set; }
+   public void Ligar() {
        ligada = true;
-    }
-    public void Desligar() {
+       System.Console.WriteLine("Ligada");
+   }
+   public void Desligar() {
        ligada = false;
+       System.Console.WriteLine("Desligada");
+   }
+}
+public class NovaAbstracao {
+   private double potencia;
+   private double tensao;
+      public double Potencia { get; set; }
+      public double Tensao { get; set; }
+         public static void Main() {
+         NovaAbstracao watts = new NovaAbstracao();
+         watts.Potencia = 100;
+         System.Console.WriteLine($"{watts.Potencia} W");
 
-    }
+         NovaAbstracao volts = new NovaAbstracao();
+         volts.Tensao = 120;
+         System.Console.WriteLine($"{volts.Tensao} V");
+      }
 }
