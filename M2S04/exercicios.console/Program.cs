3987a00e0a6639701786e0e67513b5c3525ed549
Robson De Andrade <theairelibre@protonmail.com>
2022-06-24 15:50:13 -0300

    Add namespace create methods inside classes

--- M2S04/exercicios.console/Program.cs
+++ M2S04/exercicios.console/Program.cs
@@ -1,4 +1,4 @@
-﻿/* Ex 1, 2
+﻿/* Ex 1, 2, 3
 
 Desenvolva uma abstração de uma lâmpada, a qual pode ser ligada e desligada. 
 Também deve ser possível observar o estado da lâmpada (se desligada ou ligada).
@@ -6,31 +6,57 @@ Também deve ser possível observar o estado da lâmpada (se desligada ou ligada
 Desenvolva um novo tipo de abstração para a lâmpada de forma a incluir as características de potência e voltagem. 
 Garanta que seja possível tanto ler quanto alterar os valores de potência e voltagem de uma lâmpada. 
 Essa nova classe deve ser usada como um tipo de uma propriedade da classe Lâmpada.
+
+Desenvolva um programa para testar as classes desenvolvidas nos exercícios 1 e 2. 
+Crie uma nova lâmpada, apresente no console as informações de estado (se ligada ou desligada, potência e voltagem), 
+ligue a lâmpada e apresente novamente as informações de estado.
+
 */
-public class Lampada {
-   bool ligada;
-   public NovaAbstracao MyAbstraction { get; set; }
-   public void Ligar() {
-       ligada = true;
-       System.Console.WriteLine("Ligada");
+
+namespace Eletrica {
+   public static class Lampada {
+      
+      // public NovaAbstracao MyAbstraction { get; set; }
+      // public Lampada(NovaAbstracao MyAbstraction) {
+      //    this.MyAbstraction = MyAbstraction;
+      // }
+      static void Main() {
+     
+         System.Console.WriteLine("Acender a lâmpada? (sim ou não)");
+         
+         LigadoDesligado();
+
+         System.Console.WriteLine("Apagar a lâmpada? (SIM)");
+
+         LigadoDesligado();
+
+         NovaAbstracao.WattsAndVolts();
+      
+      }
+      public static void LigadoDesligado() {
+
+         var estaLigadoOuDesligado = Console.ReadLine();
+
+         string ligadoOuDesligado = (estaLigadoOuDesligado == "sim") ?
+            
+            "Ligada! Não esqueça de desligar quando sair :)" : "Desligada";
+         
+         System.Console.WriteLine(ligadoOuDesligado);
+      }
    }
-   public void Desligar() {
-       ligada = false;
-       System.Console.WriteLine("Desligada");
-   }
-}
-public class NovaAbstracao {
-   private double potencia;
-   private double tensao;
+   public class NovaAbstracao {
       public double Potencia { get; set; }
       public double Tensao { get; set; }
-         public static void Main() {
+      public NovaAbstracao() {
+         Potencia = 100;
+         Tensao = 120;
+      }
+      public static void WattsAndVolts() {
          NovaAbstracao watts = new NovaAbstracao();
-         watts.Potencia = 100;
          System.Console.WriteLine($"{watts.Potencia} W");
 
          NovaAbstracao volts = new NovaAbstracao();
-         volts.Tensao = 120;
          System.Console.WriteLine($"{volts.Tensao} V");
       }
+   }
 }
