1eb7847fc4bc744526381380bce2b18c5362da30
Robson De Andrade <theairelibre@protonmail.com>
2022-06-28 11:17:50 -0300

    Create classes and methods

---
+++ M2S04/exercicios-parteII.console/Program.cs
@@ -0,0 +1,51 @@
+﻿/* Ex 5
+
+Faça uma classe Conta que contenha o nome do cliente, o número da conta e o saldo. 
+Estes valores deverão ser informados no construtor da classe. 
+Faça um método depositar e um método sacar para realizar o depósito e saque de valores da conta. 
+Faça um método obterSaldo que retorne o saldo do cliente. Faça um método obterNumero que retorne o número da conta. 
+Faça um método obterNomeCliente que retorna o nome do cliente titular da conta.
+
+*/
+using System;
+
+namespace Banco
+{
+    public class Conta
+    {
+        string nomeCliente;
+        int numeroConta;
+        decimal saldo;
+
+        public Conta()
+        {
+            nomeCliente = "Robson";
+            numeroConta = 1003104;
+            saldo = 25.60m;
+        }
+
+        public Depositar()
+        {
+
+        }
+        public Sacar()
+        {
+
+        }
+
+        public ObterSaldo()
+        {
+
+        }
+
+        public ObterNumero()
+        {
+
+        }
+
+        public ObterNomeCliente()
+        {
+
+        }
+    }
+}
