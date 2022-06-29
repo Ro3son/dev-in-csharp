abdb23639462e9922991a373feebdf58f5cb3180
Robson De Andrade <theairelibre@protonmail.com>
2022-06-29 11:20:20 -0300

    Add sobrecargas

--- M2S04/metodosDeExtensao.console/MetodosDeExtensao.cs
+++ M2S04/metodosDeExtensao.console/MetodosDeExtensao.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿/*
+Ex 7, 8, 9
+
+Crie uma classe estática chamada MetodosDeExtensao.cs e 
+crie um método de extensão para retornar um texto que represente o valor de um bool.
+Exemplo: 
+bool a = "false" a.*ImprimirMensagem()* // deve retornar "Falso" 
+bool a = "true" a.*ImprimirMensagem()* // deve retornar "Verdadeiro"
+
+Utilizando a classe anterior crie um método de extensão para multiplicar um inteiro (int).
+O método deve receber um parâmetro do tipo inteiro.
+
+Crie sobrecargas do método anterior como:
+receber decimal ao invés de int
+receber mais de 2 parâmetros
+receber parâmetros opcionais
+
+*/
+
+using System;
 
 namespace MetodosExtensao
 {
@@ -12,6 +31,9 @@ namespace MetodosExtensao
             Console.WriteLine(b.ImprimirMensagem());
 
             Console.WriteLine(Multiplicar(9, 9));
+            Console.WriteLine(Multiplicar(9, 9.8m));
+            Console.WriteLine(Multiplicar(5, 5.2m, 5.4m));
+            Console.WriteLine(Multiplicar(2));
 
         }
         public static bool ImprimirMensagem(this bool mensagem)
@@ -21,6 +43,15 @@ namespace MetodosExtensao
         public static int Multiplicar(this int num1, int num2)
         {
             return num1 * num2;
+        }
+        public static decimal Multiplicar(this int num1, decimal num2) {
+            return num1 * num2;
+        }
+        public static decimal Multiplicar(this int num1, decimal num2, decimal num3) {
+            return num1 * num2 * num3;
+        }
+        public static decimal Multiplicar(this int num1, int num2 = 10, int num3 = 10, decimal num4 = 20.5m) {
+            return num1 * num2 * num3 * num4;
         }
     }
 }
