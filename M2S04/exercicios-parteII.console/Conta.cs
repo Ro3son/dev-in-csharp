703881908475a0da16501652aebf9e0f509cdc6a
Robson De Andrade <theairelibre@protonmail.com>
2022-06-29 09:27:03 -0300

    Create file for class Conta

---
+++ M2S04/exercicios-parteII.console/Conta.cs
@@ -0,0 +1,46 @@
+namespace Banco
+{
+    public class Conta
+    {
+        private string nomeCliente;
+        private int numeroConta;
+        private decimal saldoConta;
+        private decimal deposito, saque;
+        private decimal aposDeposito, aposSaque;
+
+        public Conta(string nome, int conta, decimal saldo)
+        {
+            nomeCliente = nome;
+            numeroConta = conta;
+            saldoConta = saldo;
+        }
+        public void Depositar()
+        {
+            Console.WriteLine("Digite o valor para depósito: ");
+            deposito = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine($"Você depositou: {deposito} ");
+            aposDeposito = (deposito + saldoConta);
+            Console.WriteLine($"Saldo Atual: {aposDeposito} \n");
+        }
+        public void Sacar()
+        {
+            Console.WriteLine("Digite o valor para saque: ");
+            saque = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine($"Você sacou: {saque} ");
+            aposSaque = (aposDeposito - saque);
+            Console.WriteLine($"Saldo Atual: {aposSaque} \n");
+        }
+        public void ObterSaldo()
+        {
+            Console.WriteLine($"Saldo: {aposSaque}");
+        }
+        public void ObterNumero()
+        {
+            Console.WriteLine($"Número da conta: {numeroConta}");
+        }
+        public void ObterNomeCliente()
+        {
+            Console.WriteLine($"Nome do cliente: {nomeCliente}");
+        }
+    }
+}
