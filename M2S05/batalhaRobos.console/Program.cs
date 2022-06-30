7da534bbc4ff3457519ff60249befb79af0c2464
Robson De Andrade <theairelibre@protonmail.com>
2022-06-30 15:27:00 -0300

    Create new instance of the RobotBattleLight

--- M2S05/batalhaRobos.console/Program.cs
+++ M2S05/batalhaRobos.console/Program.cs
@@ -5,10 +5,21 @@ namespace ProjectBattleRobots
     {
         static void Main()
         {
-          RobotBattleHeavy robot = new RobotBattleHeavy();
-          robot.NomeRobo = "R2D2";
-          robot.PontosVida = 20;
-          robot.Status = "Desligado";
+            RobotBattleHeavy robot1 = new RobotBattleHeavy();
+            robot1.NomeRobo = "RobotOne";
+            Console.WriteLine($"Nome do Robô: {robot1.NomeRobo}");
+            robot1.PontosVida = 20;
+            Console.WriteLine($"Pontos de vida: {robot1.PontosVida}");
+            robot1.Status = "Desligado";
+            Console.WriteLine($"Status: {robot1.Status}");
+
+            RobotBattleLight robot2 = new RobotBattleLight();
+            robot2.NomeRobo = "RobotTwo";
+            Console.WriteLine($"Nome do Robô: {robot2.NomeRobo}");
+            robot2.PontosVida = 40;
+            Console.WriteLine($"Pontos de vida: {robot2.PontosVida}");
+            robot2.Status = "Desligado";
+            Console.WriteLine($"Status: {robot2.Status}");
         }
     }
 }
