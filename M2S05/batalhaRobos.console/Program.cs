4e459b07aafd767ab886d6b84663937bb5154e5a
Robson De Andrade <theairelibre@protonmail.com>
2022-06-30 11:03:52 -0300

    class Program

---
+++ M2S05/batalhaRobos.console/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ProjectBattleRobots
+{
+    class Program
+    {
+        static void Main()
+        {
+          RobotBattleHeavy robot = new RobotBattleHeavy();
+          robot.NomeRobo = "R2D2";
+          robot.PontosVida = 20;
+          robot.Status = "Desligado";
+        }
+    }
+}
