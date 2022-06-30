f87bc3a623cb7af8f788f108ae43389938ff8b27
Robson De Andrade <theairelibre@protonmail.com>
2022-06-30 15:29:46 -0300

    Create class RobotBattleLight that inherit

---
+++ M2S05/batalhaRobos.console/RobotBattleLight.cs
@@ -0,0 +1,9 @@
+
+namespace ProjectBattleRobots
+{
+    public class RobotBattleLight : Robot {
+       public override void CausarDano() {
+           int roboBatalhaLeveCausaDano = 10;
+       }
+    }
+}
