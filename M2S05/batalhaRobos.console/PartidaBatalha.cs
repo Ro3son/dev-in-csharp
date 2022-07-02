a80713330fffa75ecf52060fb94bcf9c9448d369
Robson De Andrade <theairelibre@protonmail.com>
2022-07-02 11:21:46 -0300

    Add parameters

--- M2S05/batalhaRobos.console/PartidaBatalha.cs
+++ M2S05/batalhaRobos.console/PartidaBatalha.cs
@@ -3,8 +3,9 @@ namespace ProjectBattleRobots
 {
     public class PartidaBatalha {
        
-       public void Batalhar() {
-
+       public void Batalhar(Robot robot1, Robot robot2) {
+          robot1.CausarDano();
+          robot2.CausarDano();
        }
     }
 }
