c37f262f960dfaab4dad07e8269c9e3fc369f2f2
Robson De Andrade <theairelibre@protonmail.com>
2022-07-03 11:35:03 -0300

    Add argument parameters

--- M2S05/batalhaRobos.console/Program.cs
+++ M2S05/batalhaRobos.console/Program.cs
@@ -5,38 +5,16 @@ namespace ProjectBattleRobots
     {
         static void Main()
         {
-            RobotBattleHeavy robot1 = new RobotBattleHeavy();
-            robot1.NomeRobo = "Bender";
-            robot1.PontosVida = 20;
-           
+            RobotBattleHeavy robot1 = new RobotBattleHeavy("Bender", 20);
             
-            RobotBattleLight robot2 = new RobotBattleLight();
-            robot2.NomeRobo = "R2D2";
-            robot2.PontosVida = 40;
+            RobotBattleLight robot2 = new RobotBattleLight("R2D2", 40);
+
+            PartidaBatalha.Batalhar(robot1, robot2);
          
-
-            Console.WriteLine(" ---------- Batalha Entre Robôs ---------- \n");
-
-            Console.WriteLine(
-                $"{robot1.NomeRobo} vs {robot2.NomeRobo} \n" +
-                $"Pontos de Vida: \n {robot1.PontosVida} ----- {robot2.PontosVida} \n"
-            );
-
-            Console.WriteLine("Iniciar Partida? (s/n)");
-
-            var iniciarPartida = Console.ReadLine();
-
-            if (iniciarPartida == "s") {
-                Console.WriteLine($"{robot1.NomeRobo}");
-                robot1.Iniciar();
-                Console.WriteLine($"{robot2.NomeRobo}");
-                robot2.Iniciar();
-                Console.WriteLine($"{robot2.NomeRobo} Ataca {robot1.NomeRobo}");
-                robot2.CausarDano();
-                Console.WriteLine($"{robot1.NomeRobo} Não fica parado e ataca {robot2.NomeRobo}");
-                robot1.CausarDano();
-                Console.WriteLine($"Pontos de Vida: \n {robot1.PontosVida} ----- {robot2.PontosVida} \n");
-            }
         }
     }
 }
+
+// Uma referência de objeto é necessária para o campo, 
+// o método ou a propriedade "PartidaBatalha.Batalhar(Robot, Robot)" não estática
+// Solução usar classe estática na classe PartidaBatalha.
