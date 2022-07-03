c45b97b5c1e1000f806ea69db444910154e945d7
Robson De Andrade <theairelibre@protonmail.com>
2022-07-03 11:40:15 -0300

    Pass argument to parameter, add base keyword access members

--- M2S05/batalhaRobos.console/RobotBattleHeavy.cs
+++ M2S05/batalhaRobos.console/RobotBattleHeavy.cs
@@ -3,9 +3,12 @@ namespace ProjectBattleRobots
 {
     public class RobotBattleHeavy : Robot
     {
-        public override void CausarDano()
+        public RobotBattleHeavy(string nome, int pontos) : base(nome, pontos) { }
+        public override int CausarDano()
         {
-          int roboBatalhaPesadoCausaDano = 20;
+            int roboBatalhaPesadoCausaDano = 20;
+            Console.WriteLine(roboBatalhaPesadoCausaDano);
+            return roboBatalhaPesadoCausaDano;
         }
     }
 }
