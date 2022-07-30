5e664a7652c6b58216a866aaa16278fa92d31c98
Robson De Andrade <theairelibre@protonmail.com>
2022-07-30 11:04:08 -0300

    Create new method for atacante Random

--- M2S05/batalhaRobos.console/PartidaBatalha.cs
+++ M2S05/batalhaRobos.console/PartidaBatalha.cs
@@ -1,11 +1,9 @@
-
 namespace ProjectBattleRobots
 {
     public static class PartidaBatalha
     {
         public static void Batalhar(Robot robot1, Robot robot2)
         {
-
             Console.WriteLine(" ---------- Batalha Entre Robôs ---------- \n");
 
             Console.WriteLine("Iniciar Partida? (s/n)");
@@ -15,24 +13,45 @@ namespace ProjectBattleRobots
             if (iniciarPartida == "s")
             {
                 Console.WriteLine(
-                   $"{robot1.NomeRobo} vs {robot2.NomeRobo} \n" +
-                   $"Pontos de Vida: \n {robot1.PontosVida} ----- {robot2.PontosVida} \n"
-               );
+                    $"{robot1.NomeRobo} vs {robot2.NomeRobo} \n"
+                        + $"Pontos de Vida: \n {robot1.PontosVida} ----- {robot2.PontosVida} \n"
+                );
 
                 Console.WriteLine($"{robot1.NomeRobo} ");
                 robot1.Iniciar();
                 Console.WriteLine($"{robot2.NomeRobo} ");
                 robot2.Iniciar();
                 Console.Write("\n");
-
-                Console.WriteLine($"{robot2.NomeRobo} Ataca {robot1.NomeRobo}.");
-                Console.WriteLine("Dano causado: ");
-                robot1.ReduzirPontosVida(robot2.CausarDano());
                 
-                Console.WriteLine($"{robot1.NomeRobo} Ataca {robot2.NomeRobo}. \n");
-                Console.WriteLine("Dano causado: ");
-                robot2.ReduzirPontosVida(robot1.CausarDano());
-                Console.WriteLine($"{robot1.NomeRobo} está aguardando...");
+                Atacar(robot1, robot2);
+                
+            }
+        }
+        public static void Atacar(Robot robo1, Robot robo2)
+        {
+            Random rnd = new Random();
+
+            string[] atacante = { "Bender", "R2D2", "R2D2", "Bender" };
+
+            for (int i = 0; i < 2; i++)
+            {
+                int mIndex = rnd.Next(atacante.Length);
+
+                Console.WriteLine("Atacante 1: {0}", atacante[mIndex]);
+
+                if (atacante[mIndex] == "Bender")
+                {
+                    Console.WriteLine("Atacante 2: R2D2");
+                    Console.WriteLine("Dano causado: ");
+                    robo1.ReduzirPontosVida(robo2.CausarDano());
+                }
+                else if (atacante[mIndex] == "R2D2")
+                {
+                    Console.WriteLine("Atacante 2: Bender");
+                    Console.WriteLine("Dano causado: ");
+                    robo2.ReduzirPontosVida(robo1.CausarDano());
+                    Console.WriteLine($"{atacante[mIndex]} está aguardando...");
+                }
             }
         }
     }
